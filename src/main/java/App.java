dc1b4dad9a75cff9d248df1e10c067eb3fb4a688
pav3433 <gubkin.14@yandex.ru>
2024-04-11 15:24:45 +0300

    change1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println("bean и bean1 ссылаются на один и тот же обьект?");
+        System.out.println(bean==bean1);
+        System.out.println("cat и cat1 ссылаются на один и тот же обьект?");
+        System.out.println(cat==cat1);
     }
 }
